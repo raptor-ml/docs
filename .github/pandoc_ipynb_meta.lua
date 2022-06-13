1fd7792b13f5d274c9773de7669cc7360fb24253
AlmogBaku <almog.baku@gmail.com>
2022-06-13 10:29:16 +0300

    :ghost: hiding div for docusaurus in ipynb

--- .github/pandoc_ipynb_meta.lua
+++ .github/pandoc_ipynb_meta.lua
@@ -1,13 +1,21 @@
-return {
-    {
-        Meta = function(meta)
-            if meta.jupyter.docusaurus then
-                for k, v in pairs(meta.jupyter.docusaurus) do
-                    meta[k] = v
-                end
-                meta.jupyter.docusaurus = nil
-            end
-            return meta
-        end,
-    }
-}
+-- to use the debugger, you need to have lua installed, and mobdebug installed via luarocks
+--md = require("mobdebug")
+--md.start()
+--then use md.pause() to put a breakpoint.
+
+function Meta(meta)
+    if meta.jupyter.docusaurus then
+        for k, v in pairs(meta.jupyter.docusaurus) do
+            meta[k] = v
+        end
+        meta.jupyter.docusaurus = nil
+    end
+    return meta
+end
+
+function Div(el)
+    if el.attributes["docusaurus_hide"] == "true" then
+        el.content=""
+    end
+    return el
+end
