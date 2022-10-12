b76d8302cdb24120d09f9d02e1c11206c582ff6a
AlmogBaku <almog.baku@gmail.com>
2022-10-12 20:40:10 +0300

    style: enable respectPrefersColorScheme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,9 @@ const config = {
   themeConfig:
   /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
       navbar: {
         logo: {
           alt: 'Raptor',
