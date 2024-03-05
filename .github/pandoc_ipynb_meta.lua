770997d8644f7083be6e780f23eb2312f4674492
almogbaku <almog.baku@gmail.com>
2024-03-05 14:32:45 +0200

    build: upgrade docusaurus

--- .github/pandoc_ipynb_meta.lua
+++ .github/pandoc_ipynb_meta.lua
@@ -1,7 +1,15 @@
 -- to use the debugger, you need to have lua installed, and mobdebug installed via luarocks
---md = require("mobdebug")
---md.start()
---then use md.pause() to put a breakpoint.
+-- md = require("mobdebug")
+-- md.start()
+-- then use md.pause() to put a breakpoint.
+--
+-- alternatively use the logging.lua script. make sure to run this first: export LUA_PATH="`pwd`/.github/?.lua;;"
+--
+-- local logging = require 'pandoc-logging'
+--
+-- function Pandoc(pandoc)
+--     logging.temp('pandoc', pandoc)
+-- end
 
 function Meta(meta)
     if meta.jupyter.docusaurus then
@@ -18,4 +26,12 @@ function Div(el)
         el.content=""
     end
     return el
+end
+
+-- fix a problem with <> in codeblocks
+function CodeBlock(block)
+  if block.attr.classes[1] == nil then
+    table.insert(block.attr.classes, "console")
+  end
+  return block
 end
